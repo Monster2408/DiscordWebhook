e7489b2e066d688f4a73848f720cba0bbd831360
Monster2408 <meoto2408@gmail.com>
2021-08-14 22:44:03 +0900

    aaa

---
+++ discord-webhook.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Plugin Name: DiscordWebhook
+    Plugin URI:
+    Description: 新規投稿時にDiscordにWebhookで送信
+    Version: 1.0.0
+    Author: Monster2408
+    Author URI: https://github.com/Monster2408/DiscordWebhook
+    License: GPLv2
+ */
+
+?>
